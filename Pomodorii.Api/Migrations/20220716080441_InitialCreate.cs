e568af09595b2f5a925d26205ef8110c44938faa
YvanoDelavega <66245379+YvanoDelavega@users.noreply.github.com>
2022-07-16 10:41:14 +0200

    use mysql

---
+++ Pomodorii.Api/Migrations/20220716080441_InitialCreate.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pomodorii.Api.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Tomates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nom = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Description = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ImageUrl = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tomates", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Semis",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    TomateId = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Semis", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Semis_Tomates_TomateId",
+                        column: x => x.TomateId,
+                        principalTable: "Tomates",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Tomates",
+                columns: new[] { "Id", "Description", "ImageUrl", "Nom" },
+                values: new object[,]
+                {
+                    { 1, "très bon", "img/tomates/ALAMBRA.gif", "Alambra" },
+                    { 2, "très bon", "img/tomates/Andine Cornue.gif", "Andine Cornue" },
+                    { 3, "très bon", "img/tomates/COBRA.gif", "Cobra" },
+                    { 4, "très bon", "img/tomates/coeur-de-boeuf.gif", "Coeur-de-boeuf" },
+                    { 5, "très bon", "img/tomates/noire-de-crimee.gif", "noire-de-crimee" },
+                    { 6, "très bon", "img/tomates/russe-rouge.gif", "russe-rouge" },
+                    { 7, "très bon", "img/tomates/tomito-f1.gif", "tomito-f1" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Semis",
+                columns: new[] { "Id", "Date", "TomateId" },
+                values: new object[] { 8, new DateTime(2022, 7, 16, 0, 0, 0, 0, DateTimeKind.Local), 1 });
+
+            migrationBuilder.InsertData(
+                table: "Semis",
+                columns: new[] { "Id", "Date", "TomateId" },
+                values: new object[] { 9, new DateTime(2022, 7, 16, 0, 0, 0, 0, DateTimeKind.Local), 2 });
+
+            migrationBuilder.InsertData(
+                table: "Semis",
+                columns: new[] { "Id", "Date", "TomateId" },
+                values: new object[] { 10, new DateTime(2022, 7, 14, 0, 0, 0, 0, DateTimeKind.Local), 2 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Semis_TomateId",
+                table: "Semis",
+                column: "TomateId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Semis");
+
+            migrationBuilder.DropTable(
+                name: "Tomates");
+        }
+    }
+}
--- Pomodorii.web/Program.cs
+++ Pomodorii.web/Program.cs
@@ -12,9 +12,54 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
+
 // base de données à utiliser
-builder.Services.AddDbContext<AppDbContext>(options =>
-        options.UseSqlite("Data Source=pomodorii.db"));
+//builder.Services.AddDbContext<AppDbContext>(options =>
+//        options.UseSqlite("Data Source=pomodorii.db"));
+//builder.Services.AddDbContext<AppDbContext>(options =>
+//        //options.UseSqlServer(Configuration.GetConnectionString("DBConnection")));
+//        options.UseSqlServer("server=(localdb)\\MSSQLLocalDB;database=PomodoriiDB;Trusted_Connection=true", b => b.MigrationsAssembly("Pomodorii.web")));
+
+
+// https://github.com/PomeloFoundation/Pomelo.EntityFrameworkCore.MySql
+// pour generer les classes (scaffold) à partir de la base mysql :
+// installer les outils https://docs.microsoft.com/en-us/ef/core/cli/dotnet (rq je pense qu'il n'y a pas besoin des outils si on est dans la « console du gestionnaire de package 
+// se placer dans le repertoire de l'api
+// executer : dotnet ef dbcontext scaffold "server=127.0.0.1;port=3306;user=root;password=root;database=easypingplus" "Pomelo.EntityFrameworkCore.MySql"
+
+// https://www.pragimtech.com/blog/blazor/asp.net-core-rest-api-dbcontext/
+// pour générer la bd à partir des classes
+// Il faut se mettre obligatoirement dans la « console du gestionnaire de package » et choisir « Pomodorii.Api » en projet par défaut
+//Le projet de démarrage de la solution est « Pomodorii.web »
+// ensuiite faire :
+// Add-Migration InitialCreate
+// Update-Database
+
+
+
+// Replace with your connection string.
+//var connectionString = "mysql://root:root@127.0.0.1:3306/easypingplus"; //"server=localhost;user=root;password=1234;database=ef";
+var connectionString = builder.Configuration.GetConnectionString("DBConnection");
+//var connectionString = builder.Configuration.GetConnectionString("DBConnectionMySQL");
+//var connectionString = "server=127.0.0.1;port=3306;user=root;password=root;database=easypingplus";
+//var connectionString = "server=oliadkuxrl9xdugh.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;port=3306;user=ye4z4u7mdlu8enwh;password=biql1dm5jnz2jlcg;database=c084srtqiisxknq4";
+//dotnet easypingplus dbcontext scaffold "server=127.0.0.1;port=3306;user=root;password=root;database=easypingplus"; "Pomelo.EntityFrameworkCore.MySql"
+// Replace with your server version and type.
+// Use 'MariaDbServerVersion' for MariaDB.
+// Alternatively, use 'ServerVersion.AutoDetect(connectionString)'.
+// For common usages, see pull request #1233.
+var serverVersion = ServerVersion.AutoDetect(connectionString); //new MySqlServerVersion(new Version(8, 0, 27));
+
+// Replace 'YourDbContext' with the name of your own DbContext derived class.
+builder.Services.AddDbContext<AppDbContext>(
+    dbContextOptions => dbContextOptions
+        .UseMySql(connectionString, serverVersion)//, b => b.MigrationsAssembly("Pomodorii.web"))
+        // The following three options help with debugging, but should
+        // be changed or removed for production.
+        .LogTo(Console.WriteLine, LogLevel.Information)
+        .EnableSensitiveDataLogging()
+        .EnableDetailedErrors());
+
 
 // fourni les instances utiles à l'application
 builder.Services.AddScoped<ITomateRepository, TomateRepository>();
